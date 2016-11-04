effbffb2e3a07bc0bd3641fabe0041e6ff67f1a3
Shavauhn Gabay <shavyg2@gmail.com>
2016-11-04 12:01:43 -0400

    fixed view to add parameter page input

--- Utility/View.ts
+++ Utility/View.ts
@@ -1,11 +1,14 @@
 import { Accessor } from './../win/Accessor';
-export function View(target:Object,key:string,d:TypedPropertyDescriptor<any>){
-    let original  = d.value;
 
-    d.value = function(...args:string[]){
-        this.internal_options.render = true;
-        original.apply(this,args);
+
+export function View(page:string) {
+
+    return function View(target: Object, key: string, d: TypedPropertyDescriptor<any>) {
+        let original = d.value;
+        d.value = function (...args: string[]) {
+            this.internal_options.render = true;
+            original.apply(this,page,...args);
+        }
+        return d;
     }
-    return d;
-    
 }
