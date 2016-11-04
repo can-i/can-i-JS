aa917b604da4e97754046c334a2605377aeccf84
Shavauhn Gabay <shavyg2@gmail.com>
2016-11-04 12:46:17 -0400

    changed view to have default parameters

--- Utility/View.ts
+++ Utility/View.ts
@@ -6,6 +6,7 @@ export function View(page:string) {
     return function View(target: Object, key: string, d: TypedPropertyDescriptor<any>) {
         let original = d.value;
         d.value = function (...args: string[]) {
+            args = args || [];
             this.internal_options.render = true;
             original.apply(this,page,...args);
         }
