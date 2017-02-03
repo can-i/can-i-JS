1c63ce0d1b4f5f4f39813244c1fcb49fddaddacb
Shavauhn Gabay <shavyg2@gmail.com>
2017-02-02 19:18:04 -0500

    Adding documentation for node

--- src/Framework/Contracts/FileSystem/Nodes/NodeRemover.ts
+++ src/Framework/Contracts/FileSystem/Nodes/NodeRemover.ts
@@ -1,7 +1,13 @@
-export abstract class NodeRemover {
 
+export namespace Framework.Contracts.FileSystem.Nodes {
+
+    /**
+     * This is meant to be the abstract class that will delete the node from a NodeSystem
+     */
+    export abstract class NodeRemover {
+
+        abstract delete(data:any):Promise<boolean>;
+    }
 }
 
-export default NodeRemover;
-
-
+export default Framework.Contracts.FileSystem.Nodes.NodeRemover;
