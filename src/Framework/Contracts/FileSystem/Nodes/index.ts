0dce19e5366ed447dd3906c7d3aacb65c9185091
Shavauhn Gabay <shavyg2@gmail.com>
2017-02-04 07:24:16 -0500

    added index loader for filesystem and contract nodes

---
+++ src/Framework/Contracts/FileSystem/Nodes/index.ts
@@ -0,0 +1,15 @@
+import ChildNode from "./ChildNode";
+import Node from "./Node";
+import NodeCreator from "./NodeCreator";
+import NodeInformation from "./NodeInformation";
+import NodeRemover from "./NodeRemover";
+import ParentNode from "./ParentNode";
+
+export default {
+  ChildNode,
+  Node,
+  NodeCreator,
+  NodeInformation,
+  NodeRemover,
+  ParentNode
+};
